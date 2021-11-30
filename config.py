2ec7e9527a980f882c4aab1ef5ce75bba6736e10
vekselsvip <veksel44@gmail.com>
2021-11-30 20:03:24 +0300

    bot

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '2136797405:AAFEfxXTaTWn8ZnC-FaATv76Mx7nidQtiNo'
+START = "Hello, my love. it's my wishes front sleep fo you)\npress button /sleep"
---
+++ main.py
@@ -0,0 +1,42 @@
+import telebot
+from config import *
+import time
+from flask import Flask, request
+import os
+
+
+app = Flask(__name__)
+bot = telebot.TeleBot(TOKEN)
+
+with open('wishes.txt', encoding='utf-8') as file:
+    wishes = [item.split('\n') for item in file] \
+
+
+
+@bot.message_handler(commands='start')
+def start_message(message):
+    bot.send_message(message.chat.id, START)
+
+
+@bot.message_handler(commands='sleep')
+def sleep_message(message):
+    for i in wishes:
+        bot.send_message(message.chat.id, f'<i>i[0]</i>', parse_mode='HTML')
+        time.sleep(3)
+
+
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot 25-11-2022", 200
+
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://first-my-bot.herokuapp.com/' + TOKEN)
+    return "Python Telegram Bot", 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
