66e67b5a57eb573765f4179668ad4c07498b22bb
vekselsvip <veksel44@gmail.com>
2021-11-30 20:10:36 +0300

    bot

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url='https://first-my-bot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://wishes-bot.herokuapp.com/' + TOKEN)
     return "Python Telegram Bot", 200
 
 
